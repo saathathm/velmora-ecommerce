b65b3bd0df27d687bb16958114b7bf868912243d
saathathm <saaadhath@gmail.com>
2026-05-06 13:27:52 +0530

    Refactor code structure for improved readability and maintainability

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,21 +1,24 @@
-const products = require('../data/products.json');
-const Product = require('../models/productModel');
-const dotenv = require('dotenv');
-const connectDatabase = require('../config/database')
+const products = require("../data/products.json");
+const Product = require("../models/productModel");
+const dotenv = require("dotenv");
+const path = require("path");
 
-dotenv.config({path:'backend/config/config.env'});
+dotenv.config({ path: path.join(__dirname, "../config/config.env") });
+
+const connectDatabase = require("../config/database");
+
 connectDatabase();
 
-const seedProducts = async ()=>{
-    try{
-        await Product.deleteMany();
-        console.log('Products deleted!')
-        await Product.insertMany(products);
-        console.log('All products added!');
-    }catch(error){
-        console.log(error.message);
-    }
-    process.exit();
-}
+const seedProducts = async () => {
+  try {
+    await Product.deleteMany();
+    console.log("Products deleted!");
+    await Product.insertMany(products);
+    console.log("All products added!");
+  } catch (error) {
+    console.log(error.message);
+  }
+  process.exit();
+};
 
 seedProducts();
